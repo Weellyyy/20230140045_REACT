908658a5b5fd9814394a94865414134675bb6325
Weellyyy <wildanprtm2304@gmail.com>
2025-09-04 20:48:55 +0700

    prak12

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import TodoPage from "./pages/Todo/TodoPage";
+import LoginPage from "./pages/Login/LoginPage";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/todos" element={<TodoPage />} />
       </Routes>
